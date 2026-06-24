a6a0d518d5aaf5105c0f4b93b4f4b47c01b897cd
Jiří Kerner <jirikernerjr@gmail.com>
2026-06-24 19:44:37 +0200

    Graph animation and zero point detection

--- main.c
+++ main.c
@@ -3,10 +3,16 @@
 #include <raylib.h>
 
 #define MAX_SUM 1000
-#define MAX_DISTANCE 50
+#define MAX_DISTANCE 85
+#define MAX_POINT_COUNT 2000
 #define WIDTH 900
 #define HEIGHT 800
 #define ZOOM 60
+#define ZERO_POINT_RAD 5
+#define STEP 0.05f
+#define POINTS_PER_FRAME 4
+
+int zero_count = 0;
 
 Vector2 zeta(float t)
 {
@@ -59,25 +65,92 @@ Vector2 zeta(float t)
     return (Vector2){re_zeta, im_zeta};
 }
 
+static bool point_near_center(float x, float y, float cx, float cy, float radius)
+{
+    return hypotf(x - cx, y - cy) <= radius;
+}
+
 int main()
 {
     InitWindow(WIDTH, HEIGHT, "Riemann Zeta Function");
+    SetTargetFPS(15);
+
+    int point_count = (int)(MAX_DISTANCE / STEP) + 1;
+    Vector2 points[MAX_POINT_COUNT];
+    Vector2 screen_points[MAX_POINT_COUNT];
+    bool crosses_center[MAX_POINT_COUNT] = {0};
+    bool center_marker_drawn[MAX_POINT_COUNT] = {0};
+
+    // Pre-calculate all points
+    float t = 0;
+    for (int i = 0; i < point_count; i++)
+    {
+        points[i] = zeta(t);
+
+        screen_points[i].x = points[i].x * ZOOM + WIDTH / 2;
+        screen_points[i].y = -points[i].y * ZOOM + HEIGHT / 2;
+
+        t += STEP;
+    }
+
+    bool near_center = false;
+    for (int i = 0; i < point_count; i++)
+    {
+        bool current_near_center = point_near_center(
+            screen_points[i].x, screen_points[i].y,
+            WIDTH / 2.0f, HEIGHT / 2.0f,
+            ZERO_POINT_RAD);
+
+        if (current_near_center && !near_center)
+        {
+            crosses_center[i] = true;
+            zero_count++;
+            near_center = true;
+        }
+        else if (!current_near_center)
+        {
+            near_center = false;
+        }
+    }
+
+    int drawn_points = 1;
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
+        ClearBackground(BLACK);
 
-        for (float t = 0; t <= MAX_DISTANCE; t += 0.1)
+        // Draw all calculated points
+        for (int i = 0; i < drawn_points - 1; i++)
         {
-            Vector2 pos = zeta(t);
-            float pos_x = pos.x * ZOOM;
-            float pos_y = -pos.y * ZOOM;
+            float x1 = screen_points[i].x;
+            float y1 = screen_points[i].y;
+            float x2 = screen_points[i + 1].x;
+            float y2 = screen_points[i + 1].y;
 
-            DrawPixel(pos_x + WIDTH / 2, pos_y + HEIGHT / 2, WHITE);
+            DrawLine(x1, y1, x2, y2, BLUE);
+
+            if (crosses_center[i] && !center_marker_drawn[i])
+            {
+                DrawCircle(WIDTH / 2, HEIGHT / 2, ZERO_POINT_RAD, RED);
+                center_marker_drawn[i] = true;
+            }
         }
 
         EndDrawing();
+
+        // Gradually reveal more points
+        if (drawn_points < point_count)
+        {
+            drawn_points += POINTS_PER_FRAME;
+            if (drawn_points > point_count)
+            {
+                drawn_points = point_count;
+            }
+        }
     }
+
+    CloseWindow();
 
     return 0;
 }
